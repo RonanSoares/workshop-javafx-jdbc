7883cff90f8e0e9251a8d7d0dbb5ec8c788b1a06
Ronan Soares <ronanp2@hotmail.com>
2024-01-09 21:03:43 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,144 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+	
+	// Dependêcia para o Seller. Com vistas a popular o BD e Att os dados. Implementa o método Set
+	private Seller entity;
+	
+	private SellerService service;  //Dependência
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);  // Adiciona na lista
+	}
+	
+	//Método para aplicar os eventos nos botões.
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();             //Pega os dados que estão no formulario e instancia um departamento
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();        // Pega referencia para a janela atual
+			Utils.currentStage(event).close();  // Fecha a janela
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChaged();
+		}
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");  //Instancia a exceção
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) { //Se a caixa estiver vazia
+			exception.addError("name", "O campo não pode ser vazio");                 //Lança a excessão
+		}
+		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) { //Se houber uma excessã
+			throw exception;                    //Lança a excessão
+		}
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	// Método para aplicar restrições nos objetos das janelas
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	// Para popular as caixas de textos.
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade esta vazia");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) { //Carrega os erros e percorre a coleção preenchendo os erros
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));  //Label recebe os erros.
+		}
+	}
+}
